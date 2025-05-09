a695baf615e9dad6f1086da4891e04abaac7c461
MerlinTheSpook <chrissynrick@yahoo.com>
2025-05-09 00:13:50 -0700

    Add employee

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -15,8 +15,21 @@
             employeeRick.PerformWork();
             
             double receivedWageEmployeeRick = employeeRick.ReceiveWage();
+            Console.WriteLine($"Wage paid  (message from Program): {receivedWageEmployeeRick}\n");
+
+            Console.WriteLine("Creating an Employee");
+            Console.WriteLine("--------------------");
             
-            // Additional logic for employeeRick can be added here
+            Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1980, 2, 28), 30);
+            
+            george.DisplayEmployeeDetails();
+            george.PerformWork();
+            george.PerformWork();
+            george.PerformWork(3);
+            george.PerformWork();
+            george.PerformWork(8);
+            
+            var receivedWageGeorge = george.ReceiveWage();
         }
     }
 }
